15e08420cc8570edbe73fabfe41945d4ad6acb6a
zeeshan zahoor <105010948+zeeshanzahoor786@users.noreply.github.com>
2024-06-09 00:58:25 +0200

    Updated photo URL and other changes

--- src/PhotoEditorSDK.js
+++ src/PhotoEditorSDK.js
@@ -10,7 +10,7 @@ export class PhotoEditorSDK extends React.Component {
     try {
       const editor = await PhotoEditorSDKUI.init({
         container: '#editor',
-        image: 'https://zeeshanportfolio.de/wp-content/uploads/2024/05/img-33s10-1.webp?w=1024', // Ensure this path is correct
+        image: 'https://scontent-dus1-1.xx.fbcdn.net/v/t1.6435-9/60992462_2213087365449973_2094429806778122240_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=T7FXknXsn-8Q7kNvgEWyxvd&_nc_ht=scontent-dus1-1.xx&oh=00_AYApu68K0g8dfgRlqO0SiThl2KPLQGqg4HZpngZf2JoX9A&oe=668C41DF', // Ensure this path is correct
         license: '', // Add your license key here if required
       });
       console.log('PhotoEditorSDK for Web is ready!');
